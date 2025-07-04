5912e9ebb2d3e29436970cb94557428c448597b9
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-07-04 16:44:12 +0100

    Get Mac & IP of devices on local network, and save to json for later

--- netwrk_scnr/deviceinfo.py
+++ netwrk_scnr/deviceinfo.py
@@ -1,17 +1,34 @@
-from scapy.all import ARP, Ether, srp
+import jsonlines
+import io
+from scapy.all import ARP, Ether, srp, conf
 
-def device_info(host):
+def device_info(host, iface=None):
+    try:
+        print(f"avaliable ifaces{conf.ifaces}")
+        print(f"Using interface: {iface if iface else conf.iface}")
+        arp = ARP(pdst=host)
+        broadcast = Ether (dst="ff:ff:ff:ff:ff:ff")
+        arp_request_broadcast = broadcast/arp
+        answers, unanswered = srp(arp_request_broadcast, verbose=True, timeout=5, iface=iface)
 
-    arp = ARP(pdst=host)
-    broadcast = Ether("ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp
+        devices = []
+        for element in answers:
+            device_info = {"ip" : element[1].psrc, "mac": element[1].hwsrc}
+            devices.append(device_info)
 
-    answers = srp(arp_request_broadcast, verbose = True)[0]
-
-    devices = []
-    for element in answers:
-        device_info = {"ip" : element[1].psrc, "mac": element[1].hwsrc}
-        devices.append(device_info)
-
+        if devices:    
+            print("fiound devices")
+            with jsonlines.open("devices.json", "w") as writer:
+                for device in devices:
+                    print(f"IP ADDR: {device['ip']}, MAC: {device['mac']}\n")  
+                    writer.write(device)
+        else:
+            print("no devices")
+            if unanswered:
+                print("oackets were sent but dinit answer back ")
         return devices
+    
+    except Exception as e:
+        print(f"erprr{e}")
+        return []
     
--- netwrk_scnr/main.py
+++ netwrk_scnr/main.py
@@ -1,5 +1,10 @@
 import portscn
-deviceinfo.
+import deviceinfo
+#use this file to run what you need or you can just import into own project 
 
 if __name__ == "__main__":
+    host = str(input("enter host ip address"))
+    deviceinfo.device_info(host=host, iface=None)
+
+
    
